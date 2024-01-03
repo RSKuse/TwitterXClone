8ee9424d1f986c3049828f7b4eb1535b7c7cbc8e
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2024-01-03 20:19:01 +0200

    Initial commit

---
+++ TwitterXClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterXClone
+//
+//  Created by Reuben Simphiwe Kuse on 2024/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
